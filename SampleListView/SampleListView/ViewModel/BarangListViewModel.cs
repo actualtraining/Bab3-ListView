5b9ffd530cf3fb0c39f7dfa4e5f0c4e8fa9aafc9
erick <erick@DESKTOP-JIKBK0G>
2017-03-21 13:38:00 +0700

    menambahkan BarangViewModel

---
+++ SampleListView/SampleListView/ViewModel/BarangListViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleListView.ViewModel
+{
+    public class BarangListViewModel : BindableObject
+    {
+        readonly Func<string, string, string, Task> displayAlertAction;
+        private ObservableCollection<Barang> listBarang;
+
+        public ObservableCollection<Barang> ListBarang
+        {
+            get { return listBarang; }
+            set { listBarang = value; OnPropertyChanged("ListBarang"); }
+        }
+
+        public BarangListViewModel(Func<string,string,string,Task> displayAlertAction)
+        {
+            this.displayAlertAction = displayAlertAction;
+            ListBarang = new ObservableCollection<Barang>
+            {
+                new Barang { Kode="AA001",Nama="Printer Epson 1",Stok=11,HargaBeli=2000000,HargaJual = 2200000 },
+                new Barang { Kode="AA002",Nama="Printer Epson 2",Stok=12,HargaBeli=2100000,HargaJual = 2300000 },
+                new Barang { Kode="AA003",Nama="Printer Epson 3",Stok=15,HargaBeli=2300000,HargaJual = 2500000 }
+            };
+
+            MessagingCenter.Subscribe<Barang>(this, "BuyRequested", BuyRequested);
+            MessagingCenter.Subscribe<Barang>(this, "SellRequested", (sender) =>
+            {
+                var barang = (Barang)sender;
+                displayAlertAction.Invoke("Keterangan", barang.Nama + " was clicked", "OK");
+            });
+        }
+
+        private void BuyRequested(Barang barang)
+        {
+            displayAlertAction.Invoke("Keterangan", barang.Nama + " was clicked", "OK");
+        }
+    }
+}
